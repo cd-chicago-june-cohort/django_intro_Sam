9ba296f34ca59d02d44b6102b45deb25febdde29
sozo2 <sam.zoeller@gmail.com>
2017-07-17 22:36:46 -0500

    done with book authors

--- model_practice/queries.py
+++ model_practice/queries.py
@@ -1,6 +1,8 @@
 import django
 from apps.user_login.models import *
 from apps.dojo_ninjas.models import *
+from apps.book_authors.models import *
+
 
 def sort_first_name():
     users = User.objects.order_by('first_name')
@@ -16,4 +18,22 @@ def show_first_ninjas():
     for ninja in ninjas:
         print ninja.first_name, ninja.last_name
 
+def show_books():
+    books = Book.objects.filter(all_authors__id=4)
+    for book in books:
+        print book.name 
 
+def show_authors():
+    authors = Author.objects.filter(all_books__id=2)
+    for author in authors:
+        print author.first_name, author.last_name
+
+def third_author_books():
+    books = Book.objects.filter(all_authors__id=3)
+    for book in books:
+        print book.name 
+
+def second_author_books():
+    books = Book.objects.filter(all_authors__id=2)
+    for book in books:
+        print book.name 
