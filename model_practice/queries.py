9293f81fc56d12678ae53ed3f7db03e810f16dfa
sozo2 <sam.zoeller@gmail.com>
2017-07-17 20:45:10 -0500

    done with ninja dojo exercise

--- model_practice/queries.py
+++ model_practice/queries.py
@@ -1,7 +1,19 @@
 import django
 from apps.user_login.models import *
+from apps.dojo_ninjas.models import *
 
 def sort_first_name():
     users = User.objects.order_by('first_name')
     for user in users:
         print user.first_name, user.last_name
+
+def show_first_dojo():
+    print Ninja.objects.first().dojo.name
+
+def show_first_ninjas():
+    first_doj = Dojo.objects.first()
+    ninjas = Ninja.objects.filter(dojo=first_doj)
+    for ninja in ninjas:
+        print ninja.first_name, ninja.last_name
+
+
